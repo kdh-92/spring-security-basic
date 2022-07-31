8af12072147827106ff22ed7ae18ee5fc2af1131
kdh-92 <kdh-92@gmail.com>
2022-07-31 13:30:53 +0900

    google oauth

--- src/main/java/com/codestates/security/auth/PrincipalDetails.java
+++ src/main/java/com/codestates/security/auth/PrincipalDetails.java
@@ -1,19 +1,30 @@
 package com.codestates.security.auth;
 
 import com.codestates.security.model.Member;
+import lombok.Data;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.oauth2.core.user.OAuth2User;
 
 import java.util.ArrayList;
 import java.util.Collection;
+import java.util.Map;
 
-public class PrincipalDetails implements UserDetails {
+@Data
+public class PrincipalDetails implements UserDetails, OAuth2User {
 
     private Member member;
+    private Map<String, Object> attributes;
+
 
     public PrincipalDetails(Member member) {
         this.member = member;
+    }
+
+    public PrincipalDetails(Member member, Map<String, Object> attributes) {
+        this.member = member;
+        this.attributes = attributes;
     }
 
     @Override
@@ -56,5 +67,16 @@ public class PrincipalDetails implements UserDetails {
     @Override
     public boolean isEnabled() {
         return true;
+    }
+
+
+    @Override
+    public Map<String, Object> getAttributes() {
+        return attributes;
+    }
+
+    @Override
+    public String getName() {
+        return null;
     }
 }
--- src/main/java/com/codestates/security/controller/IndexController.java
+++ src/main/java/com/codestates/security/controller/IndexController.java
@@ -1,12 +1,18 @@
 package com.codestates.security.controller;
 
+import com.codestates.security.auth.PrincipalDetails;
 import com.codestates.security.model.Member;
 import com.codestates.security.repository.MemberRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.access.annotation.Secured;
 import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.oauth2.client.userinfo.OAuth2UserRequest;
+import org.springframework.security.oauth2.core.user.OAuth2User;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -21,12 +27,19 @@ public class IndexController {
     MemberRepository memberRepository;
 
     @GetMapping("/")
-    public @ResponseBody String index() {
+    public String index(@AuthenticationPrincipal PrincipalDetails principalDetails, Model model) {
+
+        try {
+            if(principalDetails.getUsername() != null) {
+                model.addAttribute("username", principalDetails.getUsername());
+            }
+        } catch (NullPointerException e) {}
         return "index";
     }
 
     @GetMapping("/user")
-    public @ResponseBody String user() {
+    public @ResponseBody String user(@AuthenticationPrincipal PrincipalDetails principalDetails) {
+        System.out.println(principalDetails.getMember());
         return "user";
     }
 
@@ -43,6 +56,31 @@ public class IndexController {
     @GetMapping("/login")
     public String login() {
         return "loginForm";
+    }
+
+    @GetMapping("/loginTest")
+    public @ResponseBody String loginTest(Authentication authentication) {
+        System.out.println("==========/loginTest==========");
+        PrincipalDetails principalDetails = (PrincipalDetails) authentication.getPrincipal();
+        System.out.println("authentication : " + principalDetails.getMember());
+        return "로그인 세션 정보 확인";
+    }
+
+    @GetMapping("/loginTest2")
+    public @ResponseBody String loginTest2(@AuthenticationPrincipal PrincipalDetails principalDetails) {
+        System.out.println("==========/loginTest==========");
+        System.out.println("userDetails : " + principalDetails.getMember());
+        return "로그인 세션 정보 확인2";
+    }
+
+    @GetMapping("/loginTest3")
+    public @ResponseBody String loginTest3(Authentication authentication, @AuthenticationPrincipal OAuth2User oAuth) {
+        System.out.println("==========/loginOAuthTest==========");
+        OAuth2User oAuth2User = (OAuth2User) authentication.getPrincipal();
+        System.out.println("authentication : " + oAuth2User.getAttributes());
+        System.out.println("oauth2User" + oAuth.getAttributes());
+
+        return "로그인 세션 정보 확인3";
     }
 
     @GetMapping("/join")
